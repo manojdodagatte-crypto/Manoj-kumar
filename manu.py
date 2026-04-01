d971f86a810e8087e2e75138d1cca8eb27e9c3c0
manojdodagatte-crypto <manojdodagatte@gmail.com>
2026-04-01 16:02:52 +0530

    Add farewell invitation HTML template

---
+++ manu.py
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>Farewell Invitation</h2>
+
+<p>You are invited to a farewell party for <b>[Name]</b>.</p>
+<p>Date: [Date]</p>
+<p>Time: [Time]</p>
+<p>Venue: [Place]</p>
+
+<p>We hope to see you!</p>
+
+</body>
+</html>
